bb486651aca02ad169f5d35d0ea21d7b3b4c18f9
Cameron Watson <cameronw@watts-ongames.com>
2017-10-02 11:52:32 -0400

    Initial comment.

---
+++ Pack and Herd Services/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Pack and Herd Services
+//
+//  Created by Cameron Watson on 9/28/17.
+//  Copyright © 2017 Watts-On Games. All rights reserved.
+//
+
+import os.log
+import UIKit
+
+@IBDesignable class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pack and Herd ServicesTests/Pack_and_Herd_ServicesTests.swift
@@ -0,0 +1,36 @@
+//
+//  Pack_and_Herd_ServicesTests.swift
+//  Pack and Herd ServicesTests
+//
+//  Created by Cameron Watson on 9/28/17.
+//  Copyright © 2017 Watts-On Games. All rights reserved.
+//
+
+import XCTest
+@testable import Pack_and_Herd_Services
+
+class Pack_and_Herd_ServicesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
