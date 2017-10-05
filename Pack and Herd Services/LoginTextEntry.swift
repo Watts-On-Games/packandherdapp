8be300c9cd14ef307c64978499ed49e4fb6b93c9
Cameron Watson <cameronw@watts-ongames.com>
2017-10-05 11:11:47 -0400

    sync github with local xcode

---
+++ Pack and Herd Services/LoginTextEntry.swift
@@ -0,0 +1,21 @@
+//
+//  LoginTextEntry.swift
+//  Pack and Herd Services
+//
+//  Created by Cameron Watson on 10/4/17.
+//  Copyright © 2017 Watts-On Games. All rights reserved.
+//
+
+import UIKit
+
+class LoginTextEntry: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
